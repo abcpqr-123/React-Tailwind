8265cf4c54796ae8d060c3b05211a79253bd6e53
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-24 12:13:54 +0530

    just after adding the image of the burger

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <div>
+            {/* Overlay */}
+            <div>
+                <h1>The <span>Best</span></h1>
+                <h1>Foods <span>Delivered</span></h1>
+            </div>
+            <img src='https://images.pexels.com/photos/1639565/pexels-photo-1639565.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/'/>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
