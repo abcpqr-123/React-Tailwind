9ee074857d33cdb91399d7dfe88319ff506ebd57
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-25 17:30:35 +0530

    BEFORE --- // Filter Type burgers/pizza/etc

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { data } from '../data/data.js'
 
 const Food = () => {
-    console.log(data)
+    const [foods, setFoods] = useState(data)
+
+     // Filter Type burgers/pizza/etc
+     
+
     return (
         <div className='max-w-[1640px m-auto px-4 py-12'>
             <h1 className='text-orange-600 font-bold text-4xl text-center'>To Rated Menu Items</h1>
@@ -32,7 +36,22 @@ const Food = () => {
                 </div>
             </div>
 
-        {/* Display foods */}
+            {/* Display foods */}
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
+                {foods.map((item, index) => (
+                    <div className='border shadow-lg rounded-lg hover:scale-105 duration-300'>
+                        <img src={item.image} alt={item.name} 
+                        className='w-full h-[200px] object-cover rounded-t-lg'
+                        />
+                        <div className='flex justify-between px-2 py-4'>
+                            <p className='font-bold'>{item.name}</p>
+                            <p>
+                                <span className='bg-orange-500 text-white p-1 rounded-full'>{item.price}</span>
+                            </p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
